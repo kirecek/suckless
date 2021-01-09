a2dd041af5fda5483b0de5ff77edbc7808f1b7ab
Erik Jankovič <erik.jankovic@kiwi.com>
2021-01-09 18:30:48 +0100

    chore: add mem & cpu status
    Signed-off-by: Erik Jankovič <erik.jankovic@kiwi.com>

--- dwmblocks/blocks.def.h
+++ dwmblocks/blocks.def.h
@@ -2,10 +2,12 @@
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 	{"", "~/.dwm/scripts/date.sh",	30,		0},
-	{"", "~/.dwm/scripts/wifi.sh",	30,		0},
-	{"", "~/.dwm/scripts/power.sh",	30,		0},
+	{"", "~/.dwm/scripts/mem.sh",	10,		0},
+	{"", "~/.dwm/scripts/cpu.sh",	10,		0},
+	{"", "~/.dwm/scripts/wifi.sh",	60,		0},
+	{"", "~/.dwm/scripts/power.sh",	50,		0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
-static char delim[] = " | ";
-static unsigned int delimLen = 5;
+static char delim[] = "  ";
+static unsigned int delimLen = 10;
