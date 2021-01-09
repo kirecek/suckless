be71255baea8b7ed395192d3a1b714809265a48e
Erik Jankovič <erikjankovic@gmail.com>
2021-01-09 12:23:19 +0100

    chore: apply status padding patch
    Signed-off-by: Erik Jankovič <erikjankovic@gmail.com>

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -10,6 +10,8 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;        /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
+static const int horizpadbar        = 0;        /* horizontal padding for statusbar */
+static const int vertpadbar         = 15;        /* vertical padding for statusbar */
 static const char *fonts[]          = { "hack:size=12" };
 static const char dmenufont[]       = "hack:size=12";
 static const char col_bg_sel[]      = "#1d1d1d";
