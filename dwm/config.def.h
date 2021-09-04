d8ce4b6a9616feddf6b61c90e93c361611291b1a
Erik Jankovič <erikjankovic@gmail.com>
2021-09-04 11:31:16 +0200

    chore: fix rofi launcher
    Signed-off-by: Erik Jankovič <erikjankovic@gmail.com>

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -99,7 +99,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2]             = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[]       = { "dmenu_run", "-m", dmenumon, NULL };
-static const char *roficmd[]        = { "rofi", "-show", "drun", "-show-icons", NULL };
+static const char *roficmd[]        = { "rofi", "-show", "drun", "-no-default-config", "-show-icons", NULL };
 static const char *termcmd[]        = { "alacritty", NULL };
 static const char *clipmenucmd[]    = { "clipmenu", NULL };
 
